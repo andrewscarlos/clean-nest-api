6a9e9163f93f64b96a2b111da8fcf34c5ad6b945
andrewscarlos <andrews.knct@gmail.com>
2022-04-08 17:32:38 -0300

    feat: create service and controller users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
+
 
 
 @Module({
   imports: [UsersModule],
-  controllers: [],
-  providers: [],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
